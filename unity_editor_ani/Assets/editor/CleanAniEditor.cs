3f6651fee1877d6c76f061affad3a7ee7aba6dae
lights li <lightsever@hotmail.com>
2015-01-15 21:19:04 +0800

    p

---
+++ unity_editor_ani/Assets/editor/CleanAniEditor.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class CleanAniEdiotr : EditorWindow
+{
+    [MenuItem("CleanAniEdior/ShowWindow")]
+    public static void ShowCleanAni()
+    {
+        var win = EditorWindow.GetWindow<CleanAniEdiotr>();
+        win.Show();
+    }
+    GameObject selectobj = null;
+    GameObject sellast = null;
+    int toogle = 0;
+    void OnGUI()
+    {
+        if(Application.isPlaying)
+        {
+            GUILayout.Label("Can't use in playmode.");
+            return;
+        }
+        GUILayout.Label("Clean Ani Editor.");
+        {
+            var bc = GUI.backgroundColor;
+            //GUI.backgroundColor = Color.green;
+            ///选择物体
+            selectobj = EditorGUILayout.ObjectField("pickAniObj", selectobj, typeof(GameObject), GUILayout.MinHeight(1)) as GameObject;
+            {
+                EditorGUILayout.BeginHorizontal(GUILayout.MinHeight(5));
+                string set = "<null>";
+                if(Selection.activeGameObject!=null)
+                    set+=Selection.activeGameObject.name + "<" + Selection.activeGameObject.GetType().Name + ">";
+                GUILayout.Label("Active:" +set );
+                if (GUILayout.Button("use this.", GUILayout.MinHeight(1)))
+                {
+                    selectobj = Selection.activeGameObject;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            ChangeSelect();
+            EditorGUILayout.HelpBox("选择一个GameObject，要拥有Animator组件和SkindMeshRenderer组件", MessageType.Info);
+            {
+
+                toogle = GUILayout.Toolbar(toogle, new string[] { "动画", "骨骼", "上传管理" }, GUILayout.MinHeight(1));
+                EditorGUILayout.Separator();
+                EditorGUILayout.BeginHorizontal(GUILayout.MinWidth(1));
+                {
+                    GUILayout.Space(10);
+                    EditorGUILayout.BeginVertical(GUILayout.MinWidth(this.position.width-20));
+                    if (toogle == 0)
+                    {
+                        if (sellast != null)
+                        {
+                            TestAnis();
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                    EditorGUILayout.EndVertical();
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        
+
+
+        }
+    }
+    //从一个Animator中获取所有的Animation
+    void FindAllAniInControl(UnityEditorInternal.AnimatorController control, List<AnimationClip> list)
+    {
+        for (int i = 0; i < control.layerCount; i++)
+        {
+            var layer = control.GetLayer(i);
+            FindAllAniInControl(layer.stateMachine, list);
+        }
+    }
+    void FindAllAniInControl(UnityEditorInternal.StateMachine machine, List<AnimationClip> list)
+    {
+        for (int i = 0; i < machine.stateCount; i++)
+        {
+            var m = machine.GetState(i).GetMotion();
+            if (list.Contains(m as AnimationClip) == false)
+            {
+                list.Add(m as AnimationClip);
+            }
+        }
+        for (int i = 0; i < machine.stateMachineCount; i++)
+        {
+            var m = machine.GetStateMachine(i);
+            FindAllAniInControl(m, list);
+        }
+    }
+    Animator ani = null;
+    void TestAnis()
+    {
+        ani = sellast.GetComponentInChildren<Animator>();
+        Animation aniold = sellast.GetComponentInChildren<Animation>();
+        SkinnedMeshRenderer[] skins = sellast.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        GUILayout.Label("Find Ani:" + (ani != null) + ", skinmesh count=" + skins.Length);
+        if (ani == null)
+        {
+            GUILayout.Label("Need Animator Component");
+            if(aniold!=null)
+            {
+                GUILayout.Label("You have a old Animation Component.But we only support Animator.");
+
+            }
+            return;
+        }
+        List<AnimationClip> anis = new List<AnimationClip>();
+
+
+        var ac = ani.runtimeAnimatorController as UnityEditorInternal.AnimatorController;
+        FindAllAniInControl(ac, anis);
+
+
+        foreach (var a in anis)
+        {
+            if (GUILayout.Button("haveani:" + a.name, GUILayout.MinWidth(1)))
+            {
+                _selectClip = a;
+                anistart = 0;
+                aniend = a.length;
+                bPlay = false;
+                //ani.Play(a.name, 0, 0);
+            }
+        }
+        if(_selectClip)
+        {
+            var an = ani.GetCurrentAnimatorStateInfo(0);
+            anistart = GUILayout.HorizontalSlider(anistart, 0, _selectClip.length, GUILayout.MinHeight(1));
+            if (anistart >= aniend) anistart = aniend;
+            aniend = GUILayout.HorizontalSlider(aniend, 0, _selectClip.length, GUILayout.MinHeight(1));
+            if (aniend <= anistart) aniend = anistart;
+            aninow = GUILayout.HorizontalSlider(aninow, anistart, aniend, GUILayout.MinHeight(1));
+            if (!bPlay)
+            {
+                ani.Play(_selectClip.name, 0, 0);
+                ani.speed = 1.0f;
+                ani.Update(aninow);
+            }
+            bPlay = GUILayout.Toggle(bPlay, "Play", GUILayout.MaxWidth(100));
+        }
+    }
+    float anistart;
+    float aniend;
+    float aninow;
+    AnimationClip _selectClip = null;
+    bool bPlay = false;
+    void ChangeSelect()
+    {
+        if (sellast == selectobj)
+        {
+            return;
+        }
+        _selectClip = null;
+        bPlay = false;
+        sellast = selectobj;
+    }
+    void Update()
+    {
+        if (Application.isPlaying) return;
+        if(bPlay&&ani!=null&&_selectClip!=null)
+        {
+            ani.Play(_selectClip.name);
+            ani.speed = 1.0f;
+            ani.Update(0.03f);
+            Debug.Log("p"+Time.deltaTime);
+        }
+        this.Repaint();
+    }
+
+}
---
+++ unity_editor_ani/Assets/testbonetool.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class testbonetool : MonoBehaviour
+{
+    public GameObject char01;
+    // Use this for initialization
+    Animation ani;
+    SkinnedMeshRenderer skin;
+    void Start()
+    {
+        ani = char01.GetComponent<Animation>();
+        skin = char01.GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+    class MSplit
+    {
+        public Quaternion r;
+        public Vector3 t;
+        public Vector3 s;
+    }
+    MSplit[] constt = null;
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if (constt == null) constt = new MSplit[skin.bones.Length];
+        for (int i = 0; i < constt.Length; i++)
+        {
+            if (constt[i] == null) constt[i] = new MSplit();
+            constt[i].r = skin.bones[i].localRotation;
+            constt[i].t = skin.bones[i].localPosition;
+            constt[i].s = skin.bones[i].localScale;
+        }
+        //return ;
+        foreach (var iobj in listObj)
+        {
+            for (int i = 0; i < constt.Length; i++)
+            {
+                //iobj.bones[i].localPosition = constt[i].t;
+                iobj.bones[i].localRotation = constt[i].r;
+                //iobj.bones[i].localScale = constt[i].s;
+            }
+        }
+    }
+    bool bplay = true;
+    int clone100 = 0;
+    void OnGUI()
+    {
+        if (clone100 == 0)
+            if (GUI.Button(new Rect(0, 100, 100, 50), "Clone 100"))
+            {
+                clone100 = 1;
+                //ani.enabled = false;
+                for (float x = 0; x < 5; x++)
+                {
+                    for (float y = 0; y < 5; y++)
+                    {
+                        var obj = GameObject.Instantiate(char01, new Vector3(x - 2.5f, 0, y - 2.5f), Quaternion.identity);
+
+                    }
+                }
+                char01.SetActive(false);
+            }
+        if (GUI.Button(new Rect(0, 0, 100, 50), "Stop"))
+        {
+            bplay = !bplay;
+            if (bplay)
+                ani.Play();
+            else
+                ani.Stop();
+        }
+        if (clone100 == 0)
+            if (GUI.Button(new Rect(0, 50, 100, 50), "CloneChar"))
+            {
+                clone100 = 2;
+                CloneChar();
+                //skin.bones = null;
+            }
+    }
+    List<SkinnedMeshRenderer> listObj = new List<SkinnedMeshRenderer>();
+    void CloneChar()
+    {
+
+        GameObject root = new GameObject("clonechar");
+        root.transform.position = new Vector3(2, 0, 0);
+        GameObject mesh = new GameObject("mesh");
+        mesh.transform.parent = root.transform;
+        mesh.transform.localPosition = Vector3.zero;
+        SkinnedMeshRenderer mr = mesh.AddComponent<SkinnedMeshRenderer>();
+        mr.sharedMesh = skin.sharedMesh;
+        mr.sharedMaterial = skin.sharedMaterial;
+        Transform[] bonew = new Transform[skin.bones.Length];
+        GameObject rbone = new GameObject("boneRoot");
+
+        rbone.transform.parent = root.transform;
+        rbone.transform.localPosition = Vector3.zero;
+        var matr = skin.transform.worldToLocalMatrix;
+        int[] parent = new int[skin.bones.Length];
+        for (int i = 0; i < skin.bones.Length; i++)
+        {
+            parent[i] = -1;
+            for(int j=0;j<skin.bones.Length;j++)
+            {
+                if(skin.bones[j]==skin.bones[i].parent)
+                {
+                    parent[i] = j;
+                }
+            }
+            
+            var src = skin.bones[i];
+            GameObject nbone = new GameObject("bone=" + i+" parenti="+parent[i]);
+            nbone.hideFlags = HideFlags.HideInHierarchy;
+
+            bonew[i] = nbone.transform;
+
+        }
+        for (int i = 0; i < bonew.Length;i++ )
+        {
+            if (parent[i] >= 0)
+                bonew[i].parent = bonew[parent[i]];
+            else
+                bonew[i].parent = rbone.transform;
+            bonew[i].localPosition = skin.bones[i].localPosition;
+            bonew[i].localRotation = skin.bones[i].localRotation;
+            bonew[i].localScale = skin.bones[i].localScale;
+        }
+            mr.bones = bonew;
+
+        for (float x = 0; x < 5; x++)
+        {
+            for (float y = 0; y < 5; y++)
+            {
+                var obj = GameObject.Instantiate(root, new Vector3(x - 1.0f, 0, y - 1.0f), Quaternion.identity);
+                listObj.Add((obj as GameObject).GetComponentInChildren<SkinnedMeshRenderer>());
+            }
+        }
+    }
+}
